30b05743d3d9d46a13c88f8c221bbd146f6f705f
edualb <eduardoslafiq@gmail.com>
2020-01-07 13:44:43 -0300

    create repository

---
+++ SPMTest/SPMTest/SPMTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SPMTableViewController.swift
+//  SPMTest
+//
+//  Created by eduardo.silva on 07/01/20.
+//  Copyright © 2020 eduardo.silva. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+final class SPMTableViewController: UITableViewController {
+    
+}
---
+++ SPMTest/SPMTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SPMTest
+//
+//  Created by eduardo.silva on 07/01/20.
+//  Copyright © 2020 eduardo.silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
